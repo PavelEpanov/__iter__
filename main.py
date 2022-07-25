9e48663576440b61d9b2e73c397578c5e896239f
pavel <bacaaaaarttt@gmail.com>
2022-07-25 20:05:08 +0500

    second commit

--- main.py
+++ main.py
@@ -22,4 +22,26 @@ I = iter(X) # iter() вызывает __iter__
 print(next(I)) # next() вызывает __next__
 print(next(I))
 print(next(I))
-print(next(I)) # Исключение StopIteration можно перехватить с помощью try
+#print(next(I)) # Исключение StopIteration можно перехватить с помощью try
+###
+print("-" * 40)
+N = Squares(1, 5) # Создать итерируемый объект с состоянием
+aa = [n for n in N] # Израсходует элементы: __iter__ возвращает self
+print(aa)
+print([n for n in N]) # Теперь он пуст
+bb = [n for n in Squares(1, 6)] # Создать новый итерируемый объект
+print(bb)
+vv = list(Squares(1, 7)) # Новый объект для каждого нового вызова __iter__
+print(vv)
+print(36 in Squares(1, 10)) # Другие итерационные контексты
+a, b, c = Squares(1, 3) # Для каждого объекта вызывается __iter__ и затем __next__
+print(a, b, c)
+print(":".join(map(str, Squares(1, 5))))
+
+ll = Squares(1, 5)
+print(tuple(ll))
+print(tuple(ll)) # Второй вызов tuple израсходует элементы в итераторе
+gg = list(Squares(1, 5))
+print(tuple(gg))
+print(tuple(gg))
+print(tuple(gg))
